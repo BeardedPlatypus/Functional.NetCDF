5b7f454e637670439626e73f1628a46a801cd892
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-18 22:01:11 +0200

    Add initial NetCDF module and corresponding dlls

---
+++ Functional.NetCDF/NetCDF.fs
@@ -0,0 +1,21 @@
+﻿namespace BeardedPlatypus.Functional.NetCDF
+
+open System
+open System.Runtime.InteropServices
+
+/// <summary>
+/// The <see cref="NetCDF"/> module defines the external calls to the native 
+/// NetCDF dll.
+/// </summary>
+module internal NetCDF =
+    type ReturnCode = int
+
+    [<DllImport("netcdf.dll", CallingConvention = CallingConvention.Cdecl)>]
+    extern ReturnCode nc_open([<In>] string path, 
+                              [<In>] int omode,
+                              [<Out>] int& ncidp)
+
+    [<DllImport("netcdf.dll", CallingConvention = CallingConvention.Cdecl)>]
+    extern ReturnCode nc_close([<In>] int ncidp)
+
+
