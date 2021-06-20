5cce5b9542051b93efa04be1b1decf1b4ede4932
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-20 09:37:02 +0200

    Add test for nc_get_att_int

--- Functional.NetCDF.Tests/NetCDF_Test.fs
+++ Functional.NetCDF.Tests/NetCDF_Test.fs
@@ -93,6 +93,7 @@ type NetCDF_Test () =
     [<TestCase("name", "Unknown projected")>]
     [<TestCase("grid_mapping_name", "Unknown projected")>]
     [<TestCase("value", "value is equal to EPSG code")>]
+    [<TestCase("EPSG_code", "EPSG:0")>]
     [<TestCase("proj_string", "")>]
     member this.``nc_get_att_text should retrieve the correct string`` ((attributeName: string), (expectedResult: string)) =
         let mutable id : int = -1
@@ -114,3 +115,23 @@ type NetCDF_Test () =
 
         nc_close(id) |> ignore
 
+    [<Test>]
+    member this.``nc_get_att_int should retrieve the correct int array`` () =
+        let mutable id : int = -1
+        nc_open("./test-data/map.nc", NCOpenMode.NoWrite, &id) |> ignore
+        
+        let mutable t: NCType = NCType.Byte
+        let mutable l: int = -1
+
+        let varId = 0
+        let attributeName = "epsg"
+
+        nc_inq_att(id, varId, attributeName, &t, &l) |> ignore
+         
+        let resultArray = Array.create l -1s
+        let result = nc_get_att_int(id, varId, attributeName, resultArray)
+
+        result |> should equal noError
+        resultArray |> should equal [| 0s |]
+
+        nc_close(id) |> ignore
