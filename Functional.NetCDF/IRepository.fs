9592efdac135d386096cafaa18b4266c2f3842ff
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-24 21:23:23 +0200

    Add placeholder IRepository

---
+++ Functional.NetCDF/IRepository.fs
@@ -0,0 +1,10 @@
+﻿namespace BeardedPlatypus.Functional.NetCDF
+
+/// <summary>
+/// <see cref="IRepository"/> defines the interface with which to 
+/// query the different aspects of a NetCDF file. 
+/// </summary>
+[<Interface>]
+type IRepository = 
+    abstract member RetrieveVariable<'T> : name: string -> 'T
+
