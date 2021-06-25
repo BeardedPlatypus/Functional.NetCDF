9bb6018bbf80edca07705982fa8fb30fc37380fd
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-25 23:00:22 +0200

    Refactor Managed.NetCDF to create a file or throw an exception

--- Functional.NetCDF/Managed/NetCDF.fs
+++ Functional.NetCDF/Managed/NetCDF.fs
@@ -1,15 +1,17 @@
 ﻿namespace BeardedPlatypus.Functional.NetCDF.Managed
 
 open BeardedPlatypus.Functional.NetCDF.Native.NetCDF
+open BeardedPlatypus.Functional.NetCDF.Exceptions
 open Common
 
 module internal NetCDF =
     // TODO: extend open modes.
-    let OpenExisting (path: string) : Result<File, NCReturnCode> =
+    let OpenExisting (path: string) : IFile =
         let mutable id = -1
         let returnCode = nc_open(path, NCOpenMode.NoWrite, &id)
 
-        match returnCode with 
-        | NCReturnCode.NC_NOERR -> new File(NcID id) |> Result.Ok
-        | _                     -> returnCode |> Result.Error
-
+        match ErrorCode.Convert returnCode with 
+        | Some error -> 
+            raise (NetCDFException error)
+        | None -> 
+            new File(NcID id) :> IFile
