a298c42bf2732525fe2eb4426f790f2824794f4f
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-25 23:02:05 +0200

    Implement NetCDF.Query

--- Functional.NetCDF/NetCDF.fs
+++ Functional.NetCDF/NetCDF.fs
@@ -7,15 +7,15 @@
 module public NetCDF =
     /// <summary>
     /// Query the NetCDF file at <paramref name="path"/> with the specified
-    /// <paramref name="queries"/>.
+    /// <paramref name="query"/>.
     /// </summary>
     /// <param name="path">The path to the NetCDF file to query.</param>
-    /// <param name="queries">The queries to execute.</param>
+    /// <param name="query">The query to execute.</param>
     /// <exception cref="NetCDFException">
-    /// Thrown when an exception is thrown during the execution of the provided
-    /// queries.
+    /// Thrown when an exception occurs during the execution of the provided query.
     /// </exception>
-    let public Query (path: string, [<System.ParamArray>] queries: IQuery[]) : unit =
-        ()
- 
+    let public Query (path: string, query: IQuery) : unit =
+        use file: Managed.IFile = Managed.NetCDF.OpenExisting path
+        let repo: IRepository = Repository(file) :> IRepository
 
+        query.Execute repo
