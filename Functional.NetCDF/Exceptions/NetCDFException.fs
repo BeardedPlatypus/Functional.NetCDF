e6e0eb20a924c499488951f64976776867c8a6d9
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-25 22:46:43 +0200

    Add Exceptions.NetCDFException

---
+++ Functional.NetCDF/Exceptions/NetCDFException.fs
@@ -0,0 +1,7 @@
+﻿namespace BeardedPlatypus.Functional.NetCDF.Exceptions
+
+/// <summary>
+/// <see cref="NetCDFException"/> defines a NetCDF exception with the given <see cref="ReturnCode"/> 
+/// and corresponding error string.
+/// </summary>
+exception public NetCDFException of ErrorCode * string
