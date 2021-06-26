6d475354496b4cd53f39331c9fc2755e8360a9e5
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-26 17:32:52 +0200

    Add Samples projects

---
+++ Functional.NetCDF.Samples.CSharp/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Functional.NetCDF.Samples.CSharp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
