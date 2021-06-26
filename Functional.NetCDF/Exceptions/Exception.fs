ec63e94b5cbbb173132819bf7828f825edb104cb
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-26 16:40:03 +0200

    Add Exception module

---
+++ Functional.NetCDF/Exceptions/Exception.fs
@@ -0,0 +1,17 @@
+﻿namespace BeardedPlatypus.Functional.NetCDF.Exceptions
+
+open BeardedPlatypus.Functional.NetCDF.Native.NetCDF
+
+/// <summary>
+/// <see cref="Exception"/> provides some convenience functions
+/// related to the (NetCDF) exceptions.
+/// </summary>
+module internal Exception =
+    /// <summary>
+    /// Raise the default NetCDF exception
+    /// </summary>
+    let raiseDefault () = 
+        ErrorCode.convert (NCReturnCode.NC2_ERR)
+        |> NetCDFException
+        |> raise
+
