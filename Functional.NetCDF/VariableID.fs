b4acddc2c53488c4b1b1a782a70459dae031e33e
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-26 09:15:48 +0200

    Add VariableID

---
+++ Functional.NetCDF/VariableID.fs
@@ -0,0 +1,14 @@
+﻿namespace BeardedPlatypus.Functional.NetCDF
+
+open BeardedPlatypus.Functional.NetCDF.Managed.Common
+
+/// <summary>
+/// <see cref="VariableID"/> provides a tag to identify variables from within a
+/// <see cref="IRepository"/> without retrieving all data. This is necessary to 
+/// query further attributes of said variable.
+/// </summary>
+type public VariableID =
+    struct 
+        val internal ID: VarID
+        internal new(id: VarID) = { ID =id }
+    end 
