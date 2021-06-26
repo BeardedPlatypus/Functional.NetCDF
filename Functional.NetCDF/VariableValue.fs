571bf55a276b1a3df0b8c239030c1d76cdc39915
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-26 16:14:16 +0200

    Add VariableValue

---
+++ Functional.NetCDF/VariableValue.fs
@@ -0,0 +1,11 @@
+﻿namespace BeardedPlatypus.Functional.NetCDF
+
+/// <summary>
+/// <see cref="VariableValue"> implements <see cref="IVariableValue"/> interface.
+/// </summary>
+[<Sealed>]
+type internal VariableValue<'T> (values: 'T[], shape: int[]) =
+    interface IVariableValue<'T> with
+        override this.Values with get() = values |> Array.toSeq 
+        override this.Shape with get() = shape |> Array.toSeq
+
