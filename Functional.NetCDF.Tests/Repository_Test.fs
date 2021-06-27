686f96ca7cc741a53b0b52f4899feb2851579622
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-27 16:03:14 +0200

    Add additional RetrieveVariableAttribute tests

--- Functional.NetCDF.Tests/Repository_Test.fs
+++ Functional.NetCDF.Tests/Repository_Test.fs
@@ -69,3 +69,65 @@ type Repository_Test () =
         let expectedTimeSteps : seq<int> = seq { 1; 2; 1; 3; 2; 4; 2; 5; 3; 5; }
         (Seq.take 10 result.Values) |> should equal expectedTimeSteps
 
+    static member GetRetrieveAttributeValueString () : seq<TestCaseData> = 
+        seq { TestCaseData("mesh2d", "cf_role", seq { 1 }, seq { "mesh_topology" }) }
+
+    [<Test>]
+    [<TestCaseSource("GetRetrieveAttributeValueString")>]
+    member this.``RetrieveVariableAttribute with a Name returns the correct values for strings.`` (variableName: string,
+                                                                                                attributeName: string,
+                                                                                                expectedShape : seq<int>,
+                                                                                                expectedValue : seq<string>) =
+        // Setup
+        use file: Managed.IFile = Managed.NetCDF.OpenExisting "./test-data/map.nc"
+        let repo : IRepository = Repository(file) :> IRepository
+
+        // Call
+        let result = repo.RetrieveVariableAttribute<string> (variableName, attributeName)
+
+        // Assert
+        result.Shape |> should equal (expectedShape)
+
+        result.Values |> should equal expectedValue
+        
+    static member GetRetrieveAttributeValueInt () : seq<TestCaseData> = 
+        seq { TestCaseData("mesh2d", "topology_dimension", seq { 1 }, seq { 2 }) }
+
+    [<Test>]
+    [<TestCaseSource("GetRetrieveAttributeValueInt")>]
+    member this.``RetrieveVariableAttribute with a Name returns the correct values for ints.`` (variableName: string,
+                                                                                             attributeName: string,
+                                                                                             expectedShape : seq<int>,
+                                                                                             expectedValue : seq<int>) =
+        // Setup
+        use file: Managed.IFile = Managed.NetCDF.OpenExisting "./test-data/map.nc"
+        let repo : IRepository = Repository(file) :> IRepository
+
+        // Call
+        let result = repo.RetrieveVariableAttribute<int> (variableName, attributeName)
+
+        // Assert
+        result.Shape |> should equal (expectedShape)
+
+        result.Values |> should equal expectedValue
+        
+    static member GetRetrieveAttributeValueDouble () : seq<TestCaseData> = 
+        seq { TestCaseData("mesh2d+czu", "_FillValue", seq { 1 }, seq { -999.0 }) }
+
+    [<Test>]
+    [<TestCaseSource("GetRetrieveAttributeValueDouble")>]
+    member this.``RetrieveVariableAttribute with a Name returns the correct values for doubles.`` (variableName: string,
+                                                                                                attributeName: string,
+                                                                                                expectedShape : seq<int>,
+                                                                                                expectedValue : seq<double>) =
+        // Setup
+        use file: Managed.IFile = Managed.NetCDF.OpenExisting "./test-data/map.nc"
+        let repo : IRepository = Repository(file) :> IRepository
+
+        // Call
+        let result = repo.RetrieveVariableAttribute<double> (variableName, attributeName)
+
+        // Assert
+        result.Shape |> should equal (expectedShape)
+
+        result.Values |> should equal expectedValue
