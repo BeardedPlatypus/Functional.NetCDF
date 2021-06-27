983dc788d10a4333f61175731d9c8dd57886df37
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-27 08:27:25 +0200

    Add Exception_Test

---
+++ Functional.NetCDF.Tests/Exceptions/Exception_Test.fs
@@ -0,0 +1,16 @@
+﻿namespace BeardedPlatypus.Functional.NetCDF.Tests.Exceptions
+
+open NUnit.Framework
+open FsUnit
+
+open BeardedPlatypus.Functional.NetCDF.Exceptions
+
+[<TestFixture>]
+type Exception_Test () = 
+    [<Test>]
+    member this.``raiseDefault throws correct exception`` () =
+        let exc = Assert.Throws<NetCDFException>(TestDelegate(Exception.raiseDefault))
+        
+        exc.Data0 |> should equal ErrorCode.Undefined
+        exc.Data1 |> should equal "Undefined error"
+
