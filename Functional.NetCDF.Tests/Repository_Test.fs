bb1b0c2ebb13543feee8505b28d97bb1ccc23a7d
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-27 14:43:47 +0200

    Add tests for RetrieveVariableValue ints

--- Functional.NetCDF.Tests/Repository_Test.fs
+++ Functional.NetCDF.Tests/Repository_Test.fs
@@ -9,7 +9,7 @@ open BeardedPlatypus.Functional.NetCDF.Managed
 [<TestFixture>]
 type Repository_Test () =
     [<Test>]
-    member this.``RetrieveVariableValue with a VariableID returns the correct values.`` () =
+    member this.``RetrieveVariableValue with a VariableID returns the correct values for doubles.`` () =
         // Setup
         use file: Managed.IFile = Managed.NetCDF.OpenExisting "./test-data/map.nc"
         let repo : IRepository = Repository(file) :> IRepository
@@ -25,7 +25,7 @@ type Repository_Test () =
         
 
     [<Test>]
-    member this.``RetrieveVariableValue with a Name returns the correct values.`` () =
+    member this.``RetrieveVariableValue with a Name returns the correct values for doubles.`` () =
         // Setup
         use file: Managed.IFile = Managed.NetCDF.OpenExisting "./test-data/map.nc"
         let repo : IRepository = Repository(file) :> IRepository
@@ -39,3 +39,32 @@ type Repository_Test () =
         let expectedTimeSteps : seq<double> = seq { for i in 0 .. 72 -> (double) i * 1200.0 }
         result.Values |> should equal expectedTimeSteps
 
+    [<Test>]
+    member this.``RetrieveVariableValue with a VariableID returns the correct values for ints.`` () =
+        // Setup
+        use file: Managed.IFile = Managed.NetCDF.OpenExisting "./test-data/map.nc"
+        let repo : IRepository = Repository(file) :> IRepository
+
+        // Call
+        let result = repo.RetrieveVariableValue<int> (VariableID(Common.VarID 9))
+
+        // Assert
+        result.Shape |> should equal (seq { 60; 2 })
+
+        let expectedTimeSteps : seq<int> = seq { 1; 2; 1; 3; 2; 4; 2; 5; 3; 5; }
+        (Seq.take 10 result.Values) |> should equal expectedTimeSteps
+
+    [<Test>]
+    member this.``RetrieveVariableValue with a Name returns the correct values for ints.`` () =
+        // Setup
+        use file: Managed.IFile = Managed.NetCDF.OpenExisting "./test-data/map.nc"
+        let repo : IRepository = Repository(file) :> IRepository
+
+        // Call
+        let result = repo.RetrieveVariableValue<int> "mesh2d_edge_faces"
+
+        // Assert
+        result.Shape |> should equal (seq { 60; 2 })
+
+        let expectedTimeSteps : seq<int> = seq { 1; 2; 1; 3; 2; 4; 2; 5; 3; 5; }
+        (Seq.take 10 result.Values) |> should equal expectedTimeSteps
