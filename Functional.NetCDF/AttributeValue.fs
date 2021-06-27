3bdd05b12f4ccfbb5cb5b81abe89ed48d9f07122
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-27 15:42:54 +0200

    Add AttributeValue

---
+++ Functional.NetCDF/AttributeValue.fs
@@ -0,0 +1,11 @@
+﻿namespace BeardedPlatypus.Functional.NetCDF
+
+/// <summary>
+/// <see cref="AttributeValue"> implements <see cref="IAttributeValue"/> interface.
+/// </summary>
+[<Sealed>]
+type internal AttributeValue<'T> (values: 'T[], shape: int[]) =
+    interface IAttributeValue<'T> with
+        override this.Values with get() = values |> Array.toSeq 
+        override this.Shape with get() = shape |> Array.toSeq
+
---
+++ Functional.NetCDF/IAttributeValue.fs
@@ -0,0 +1,22 @@
+﻿namespace BeardedPlatypus.Functional.NetCDF
+
+/// <summary>
+/// <see cref="IAttributeValue"> defines the result of an attribute value query.
+/// 
+/// The value of an attribute can have a higher dimension. The <see cref="Shape"/>
+/// defines the number of dimensions and their corresponding lengths.
+/// The values are stored as a single ordered sequence based on the order of 
+/// dimensions defined in the shame.
+/// </summary>
+[<Interface>]
+type public IAttributeValue<'T> = 
+    /// <summary>
+    /// The values as a flattened sequence.
+    /// </summary>
+    abstract Values : seq<'T> with get
+
+    /// <summary>
+    /// The dimensions of the variable values, where each index corresponds
+    /// with a dimension of the variable result.
+    /// </summary>
+    abstract Shape : seq<int> with get
